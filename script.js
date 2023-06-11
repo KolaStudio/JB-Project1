73cf581751d071f5439fd40ebf566052702343ec
Kola <kol.ayelet@gmail.com>
2023-06-11 20:29:12 +0300

    clean version before Mobile and final tests

--- script.js
+++ script.js
@@ -16,84 +16,29 @@ const allNotesContainer = document.getElementById("notesContainer");
 const recycleContainer = document.getElementById("recycleContainer");
 const blackScreen = document.getElementById("blackScreen");
 
-let recycleList = document.getElementById("recycleList");
+// let recycleList = document.getElementById("recycleList");
 let cssVariables = document.querySelector(':root');
 
+onLoadPage()
+
+// =========================== On Load =========================== //
 function onLoadPage(){
     const settings_str = localStorage.getItem(SETTINGS_StorageKey);
     const notes_str = localStorage.getItem(NOTES_StorageKey);
     const recycle_str = localStorage.getItem(RECYCLE_StorageKey);  
-
-    if(settings_str){ settingsObj = JSON.parse(settings_str); }
-    if(notes_str){ notesArr = JSON.parse(notes_str); }
+    
+    notes_str? notesArr = JSON.parse(notes_str) : showPopUp("welcome");
     if(recycle_str){ recycleArr = JSON.parse(recycle_str); }
+    if(settings_str){ settingsObj = JSON.parse(settings_str); }
 
     cssVariables.style.setProperty('--main', settingsObj.background);
+    document.getElementById("colorInput").value = settingsObj.background;
     
     displayNotes();
     displayRecycleList();
 }
 
-function showPopUp(popUp){
-    const popUpBox = document.getElementById(popUp);
-    popUpBox.style.display = "block";
-    blackScreen.style.display = "block";
-}
-function closePopUp(popUp){
-    const popUpBox = document.getElementById(popUp);
-    popUpBox.style.display = "none";
-    blackScreen.style.display = "none";
-    clearForm();
-}
-
-function formValidation(){
-    event.preventDefault();
-    const taskText = taskTextBox.value;
-    const dateInput = dateInputBox.value;
-    const timeInput = timeInputBox.value;
-
-    if(taskText===""){
-        taskTextBox.style.backgroundColor = "#fd1e1e";
-        return;
-    }
-    if(dateInput===""){
-        dateInputBox.style.backgroundColor = "#fd1e1e";
-        return;
-    }
-    if(timeInput===""){
-        timeInputBox.style.backgroundColor = "#fd1e1e";
-        return;
-    }
-    createNote();
-}
-
-function createNote(){
-    const note = {
-        task: taskTextBox.value,
-        date: dateInputBox.value,
-        time: timeInputBox.value,
-        create: new Date(),
-        visual: {
-            rotate: (Math.random() * (5 + 5)) -5,
-            scotch: Math.floor(Math.random() * 5)+1,
-            scotchPos: Math.floor(Math.random() * (140 - 50)) + 50
-        }
-    }
-    notesArr.push(note);
-    saveToLocalStorage();
-    displayNotes();
-    clearForm();
-    document.getElementById("popUpDivForm").style.display = "none";
-    blackScreen.style.display = "none";
-}
-
-function saveToLocalStorage(){
-    const notes_str = JSON.stringify(notesArr);
-    const recycle_str = JSON.stringify(recycleArr);
-    localStorage.setItem(NOTES_StorageKey, notes_str);
-    localStorage.setItem(RECYCLE_StorageKey, recycle_str);
-}
-
+// ========================== Draw Elements =========================== //
 function displayNotes(){ 
     if(settingsObj.sortByDueDate){
         notesArr.sort(function(a, b){return new Date(a.date) - new Date(b.date)});
@@ -140,14 +85,68 @@ function displayRecycleList(){
         <td>${recycleArr[i].task}</td>
         <td><div class="restoreBtn" onclick="restoreNote(${i})"></div</td>
         </tr>`;
-    }
-       
+    }  
     recycleContainer.innerHTML = html;
 
     const recycleBinIcon = document.getElementById("recycleIcon");
-    if(recycleArr.length > 0){ recycleBinIcon.style.backgroundPosition = "0px 50px"; }
-    else { recycleBinIcon.style.backgroundPosition = "0px 0px"; }
+    if(recycleArr.length > 0){ 
+        recycleBinIcon.style.backgroundImage = "url('assets/images/recycleFull_icon.png')"; 
+    }else { 
+        recycleBinIcon.style.backgroundImage = "url('assets/images/recycleEmpty_icon.png')";
+    }
 }
+
+// ========================= New Task Creation ========================== //
+function formValidation(){
+    event.preventDefault();
+    const taskText = taskTextBox.value;
+    const dateInput = dateInputBox.value;
+    const timeInput = timeInputBox.value;
+
+    if(taskText===""){
+        taskTextBox.style.backgroundColor = "#fd1e1e";
+        return;
+    }
+    if(dateInput===""){
+        dateInputBox.style.backgroundColor = "#fd1e1e";
+        return;
+    }
+    if(timeInput===""){
+        timeInputBox.style.backgroundColor = "#fd1e1e";
+        return;
+    }
+    createNote();
+}
+
+function createNote(){
+    const note = {
+        task: taskTextBox.value,
+        date: dateInputBox.value,
+        time: timeInputBox.value,
+        create: new Date(),
+        visual: {
+            rotate: (Math.random() * (5 + 5)) -5,
+            scotch: Math.floor(Math.random() * 5)+1,
+            scotchPos: Math.floor(Math.random() * (140 - 50)) + 50
+        }
+    }
+    notesArr.push(note);
+    saveToLocalStorage();
+    displayNotes();
+    clearForm();
+    document.getElementById("popUpDivForm").style.display = "none";
+    blackScreen.style.display = "none";
+}
+
+// ===================== Save Tasks to Local Storage ====================== //
+function saveToLocalStorage(){
+    const notes_str = JSON.stringify(notesArr);
+    const recycle_str = JSON.stringify(recycleArr);
+    localStorage.setItem(NOTES_StorageKey, notes_str);
+    localStorage.setItem(RECYCLE_StorageKey, recycle_str);
+}
+
+// ===================== Recycle Bin ====================== //
 function moveNoteToRecycle(noteID){
     recycleArr.push(notesArr[noteID]);
     notesArr.splice(noteID, 1);
@@ -167,10 +166,21 @@ function emptyBin(){
     saveToLocalStorage();
     displayRecycleList();
 }
-function editNote(noteID){
-    //Open popup form with note data for edit
+
+// ===================== PopUp Boxes (show/hide) ====================== //
+function showPopUp(popUp){
+    const popUpBox = document.getElementById(popUp);
+    popUpBox.style.display = "block";
+    blackScreen.style.display = "block";
+}
+function closePopUp(popUp){
+    const popUpBox = document.getElementById(popUp);
+    popUpBox.style.display = "none";
+    blackScreen.style.display = "none";
+    clearForm();
 }
 
+// ===================== Visual and Settings ====================== //
 function clearForm(){
     taskTextBox.value = "";
     dateInputBox.value = "";
@@ -188,18 +198,19 @@ function changeColor(color){
     localStorage.setItem(SETTINGS_StorageKey, JSON.stringify(settingsObj));
 }
 function sortNotes(){
+    const sortIcon = document.getElementById("sortIcon");
     if(settingsObj.sortByDueDate){
         settingsObj.sortByDueDate = false;
+        sortIcon.style.backgroundImage = "url(assets/images/sort_icon.png)";
     }else{
         settingsObj.sortByDueDate = true;
+        sortIcon.style.backgroundImage = "url(assets/images/sortDue_icon.png)";
     }
     localStorage.setItem(SETTINGS_StorageKey, JSON.stringify(settingsObj));
     displayNotes();
 }
-
 function formatDate(d){
     const date = new Date(d);
-
     const month = date.getMonth();
     const day = date.getDate();
     const year = date.getFullYear();
